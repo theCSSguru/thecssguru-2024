20fa78ed22d4f4db82a4e7dcadc0f8e99f08a7b9
Chris Johnson <chris@thecssguru.com>
2026-02-22 16:31:44 -0500

    removed all mix-blend-modes for performance and added punchier oklch colors for gradients

--- src/@/components/Space/Space.tsx
+++ src/@/components/Space/Space.tsx
@@ -2,8 +2,8 @@ export const Space = () => {
   return (
     <div className="space-wrap fixed inset-0 z-0">
       <div className="space-bg before:bg-[url('/bgspace.jpg')] after:bg-[url('/bg.jpg')]"></div>
-      <div className="bg-brand-radial absolute inset-0 z-20 bg-cover mix-blend-soft-light"></div>
-      <div className="bg-brand-radial absolute inset-0 z-30 bg-cover opacity-50 mix-blend-overlay"></div>
+      <div className="bg-brand-radial absolute inset-0 z-20 bg-cover opacity-35"></div>
+      <div className="bg-brand-radial absolute inset-0 z-30 bg-cover opacity-65"></div>
       <div className="absolute inset-0 z-40">
         <div className="relative mx-auto max-w-screen-2xl">
           <div className="absolute left-[70rem] top-[-20rem] flex h-[800dvh] origin-top-left rotate-[70deg] gap-4 opacity-10 lg:top-[-10rem]">
