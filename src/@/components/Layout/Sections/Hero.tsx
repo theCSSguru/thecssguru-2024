bee9c9ea2100949708df369336ad63ad1abf49b7
Chris Johnson <chris@thecssguru.com>
2026-02-22 21:57:47 -0500

    cool like that

--- src/@/components/Layout/Sections/Hero.tsx
+++ src/@/components/Layout/Sections/Hero.tsx
@@ -3,13 +3,12 @@ import { Button } from '@/components/Button/Button';
 import { Social } from '@/components/Social/Social';
 import { SectionScroll } from '@/components/Layout/SectionScroll';
 import { FaExternalLinkAlt, FaRegFilePdf } from 'react-icons/fa';
-import Image from 'next/image';
 
 export const Hero = () => {
   return (
-    <SectionScroll id="Home" className="lg:py-12">
-      <div className="absolute -top-16 bottom-0 left-0 right-0 flex w-full flex-col justify-evenly overflow-x-clip lg:relative lg:bottom-0 lg:mb-0 lg:flex-row">
-        <div className="order-2 mx-auto flex max-w-full items-center px-8 lg:relative lg:order-1 lg:m-0">
+    <SectionScroll id="Home" className="lg:pb-32">
+      <div className="relative flex h-full w-full flex-col justify-evenly overflow-x-clip lg:relative lg:mb-0 lg:flex-row">
+        <div className="order-2 mx-auto -mt-32 flex max-w-full items-center lg:relative lg:order-1 lg:m-0">
           <div className="text-shadow flex w-full flex-col gap-1 lg:gap-2">
             <div className="flex gap-2 pl-1 text-sm font-medium uppercase lg:text-lg">
               <div className="font-bold text-brand-primary">Chris Johnson</div>
@@ -62,16 +61,13 @@ export const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="hero-img-bg relative -z-10 order-1 -mb-32 w-[100dvw] lg:relative lg:top-0 lg:order-2 lg:m-0 lg:w-max">
-          <Image
-            src="/me.jpg"
-            width={542}
-            height={813}
-            alt="Chris Johnson"
-            className="relative mx-auto aspect-square object-cover lg:max-w-full"
-            priority
-            unoptimized
-          />
+        <div className="hero-img-bg -z-10 order-1 mx-auto -mt-16 flex w-full max-w-[542px] flex-col justify-center lg:top-0 lg:order-2 lg:m-0">
+          <div className="relative aspect-square">
+            <div className="me bg-[url('/me.jpg')]"></div>
+            <div className="tech-border">
+              <div className="me-stylish bg-[url('/me3.jpg')]"></div>
+            </div>
+          </div>
         </div>
       </div>
     </SectionScroll>
